f0e88c6af25d71135d8f5627a56edbe11b08c726
sophie860516 <97041003+sophie860516@users.noreply.github.com>
2026-04-30 11:10:29 -0400

    add click to allow parameter input through cmd

--- working_with_uv/NYC_TAXI_DATA_V1.py
+++ working_with_uv/NYC_TAXI_DATA_V1.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
+#The script reads data in chunks to avoid memory issues while reading large csv files. 
+# The data is then inserted into a PostgreSQL database.
 
 
 from calendar import month
 
+import click
 import pandas as pd
 from tqdm.auto import tqdm
 from sqlalchemy import create_engine
@@ -35,18 +38,24 @@ parse_dates = [
 
 
 
-#assgine parameters 
-year=2021
-month=1
-chunksize= 10000
 
-def run():
+
+@click.command()
+@click.option('--year', default=2021, type=int, help='Year for the data')
+@click.option('--month', default=1, type=int, help='Month for the data')
+@click.option('--chunksize', default=10000, type=int, help='Chunk size for reading CSV')
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-password', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database')
+def run(year, month, chunksize, pg_user, pg_password, pg_host, pg_port, pg_db):
     #ingest data in csv file into postgres
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
 
     #create engine for connection
-    engine = create_engine('postgresql+psycopg://root:root@localhost:5432/ny_taxi')
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
 
     #create iterator
     df_iter = pd.read_csv(
@@ -71,4 +80,15 @@ if __name__ == "__main__":
 
 
 
-
+"""
+Example usage of passing parameters to the script through command line:
+uv run NYC_TAXI_DATA_V1.py \
+    --year=2021 \
+    --month=1 \
+    --chunksize=10000 \
+    --pg-user=root \
+    --pg-password=root \
+    --pg-host=localhost \
+    --pg-port=5432 \
+    --pg-db=ny_taxi
+"""
